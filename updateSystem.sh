6e86832c2ef746c9093a8261a5c2e661f03fe110
Can Sürmeli <c_surmeli@icloud.com>
2019-03-14 09:26:12 +0300

    Switched the status message printing to a global function for better modularization.

--- updateSystem.sh
+++ updateSystem.sh
@@ -1,7 +1,6 @@
 updateSystem() {
 	# Grab sudo
-	printStatusMessage "hello"
-	echo -e "\e[34mSTATUS: Later on, while updating PIP, sudo privileges will be required. Grant it now so that you can leave these processes on their own.\033[0m"
+	printStatusMessage "Later on, while updating PIP, sudo privileges will be required. Grant it now so that you can leave these processes on their own."
 	sudo -v
 
 	# Keep the sudo privileges alive until everything finishes
@@ -9,52 +8,52 @@ updateSystem() {
 	printNewLine
 
 	# Homebrew
-	echo -e "\e[34mSTATUS: Updating Homebrew...\033[0m"
+	printStatusMessage "Updating Homebrew..."
 	brewUpdate=$(brew update)
 	checkCommandStatus
 	printNewLine
 
-	echo -e "\e[34mSTATUS: Upgrading Homebrew...\033[0m"
+	printStatusMessage "Upgrading Homebrew..."
 	brew upgrade
 	checkCommandStatus
 	printNewLine
 
-	echo -e "\e[34mSTATUS: Cleaning up Homebrew...\033[0m"
+	printStatusMessage "Cleaning up Homebrew..."
 	brew cleanup
 	checkCommandStatus
 	printNewLine
 
 	# macOS
-	echo -e "\e[34mSTATUS: Upgrading outdated App Store apps via mas...\033[0m"
+	printStatusMessage "Upgrading outdated App Store apps via mas..."
 	mas upgrade
 	checkCommandStatus
 	printNewLine
 
 	# PIP
-	echo -e "\e[34mSTATUS: Upgrading the PIP itself...\033[0m"
+	printStatusMessage "Upgrading the PIP itself..."
 	sudo pip install --upgrade pip
 	checkCommandStatus
 	printNewLine
 
-	echo -e "\e[34mSTATUS: Updating the installed packages...\033[0m"
+	printStatusMessage "Updating the installed packages..."
 	echo -e "\e[31mFULL DISCLOSURE: Due to PIP's nature, you might have to deal with environment errors.\033[0m"
 	sudo pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U
 	checkCommandStatus
 	printNewLine
 
 	# RUBY
-	echo -e "\e[34mSTATUS: Updating Ruby itself...\033[0m"
+	printStatusMessage "Updating Ruby itself..."
 	gem update --system
 	checkCommandStatus
 	printNewLine
 
-	echo -e "\e[34mSTATUS: Updating the installed gems...\033[0m"
+	printStatusMessage "Updating the installed gems..."
 	echo -e "\e[31mFULL DISCLOSURE: It might(most probably will) take a while so take a break(you've been warned)!\033[0m"
 	gem update
 	checkCommandStatus
 	printNewLine
 
-	echo -e "\e[34mSTATUS: Cleaning up the installed gems...\033[0m"
+	printStatusMessage "Cleaning up the installed gems..."
 	gem cleanup
 	checkCommandStatus
 	printNewLine
