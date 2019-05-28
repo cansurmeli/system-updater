e9c6d395b621771cd3c2cb148a1ae138edfa2d85
Can Sürmeli <c_surmeli@icloud.com>
2019-05-28 14:18:36 +0200

    Added the ability to parse the command line arguments passed to the update script.
    However, the parsing functionality doesn't seem to be working at the
    moment even though it works by itself in a separate file.e

--- updateSystem.sh
+++ updateSystem.sh
@@ -1,4 +1,6 @@
 updateSystem() {
+	parseCommandLineArguments
+
 	sudoStatus=$(checkSudoPrivileges)
 
 	# Grab sudo if needed
@@ -93,6 +95,43 @@ checkSudoPrivileges() {
 	else
 		echo "no_sudo"
 	fi
+}
+
+parseCommandLineArguments() {
+	POSITIONAL=()
+	while [[ $# -gt 0 ]]
+	do
+	key="$1"
+
+	case $key in
+			-h|--help)
+			echo "Displaying the help manual"
+			shift # past argument
+			shift # past value
+			;;
+			-o|--output)
+			LOG_OUTPUT_LOCATION="$2"
+			shift # past argument
+			shift # past value
+			;;
+			-v|--verbose)
+			echo "Acting verbose..."
+			shift # past argument
+			shift # past value
+			;;
+			--default)
+			DEFAULT=YES
+			shift # past argument
+			;;
+			*)    # unknown option
+			POSITIONAL+=("$1") # save it in an array for later
+			shift # past argument
+			;;
+	esac
+	done
+	set -- "${POSITIONAL[@]}" # restore positional parameters
+
+	#echo LOG_OUTPUT_LOCATION = "${LOG_OUTPUT_LOCATION}"
 }
 
 updateSystem
