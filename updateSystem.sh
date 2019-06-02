5e9f8f107c7a68b3af2fc25543dc028871d6c419
Can Sürmeli <c_surmeli@icloud.com>
2019-06-02 09:41:35 +0200

    Minor cosmetic changes to the command line argument parsing function. Still not functioning though.

--- updateSystem.sh
+++ updateSystem.sh
@@ -101,37 +101,38 @@ parseCommandLineArguments() {
 	POSITIONAL=()
 	while [[ $# -gt 0 ]]
 	do
-	key="$1"
+		key="$1"
 
-	case $key in
-			-h|--help)
-			less "help.txt"
-			shift # past argument
-			shift # past value
-			;;
-			-o|--output)
-			LOG_OUTPUT_LOCATION="$2"
-			shift # past argument
-			shift # past value
-			;;
-			-v|--verbose)
-			echo "Acting verbose..."
-			shift # past argument
-			shift # past value
-			;;
-			--default)
-			DEFAULT=YES
-			shift # past argument
-			;;
-			*)    # unknown option
-			POSITIONAL+=("$1") # save it in an array for later
-			shift # past argument
-			;;
-	esac
+		case $key in
+				-h|--help)
+				HELP="$2"
+				less "help.txt"
+				shift												# past argument
+				shift												# past value
+				;;
+				-o|--output)
+				LOG_OUTPUT_LOCATION="$2"
+				shift												# past argument
+				shift												# past value
+				;;
+				-v|--verbose)
+				echo "Acting verbose..."
+				shift												# past argument
+				shift												# past value
+				;;
+				--default)
+				DEFAULT=YES
+				shift												# past argument
+				;;
+				*)													# unknown option
+				POSITIONAL+=("$1")					# save it in an array for later
+				shift												# past argument
+				;;
+		esac
 	done
-	set -- "${POSITIONAL[@]}" # restore positional parameters
+	set -- "${POSITIONAL[@]}"					# restore positional parameters
 
-	#echo LOG_OUTPUT_LOCATION = "${LOG_OUTPUT_LOCATION}"
+	echo LOG_OUTPUT_LOCATION = "${LOG_OUTPUT_LOCATION}"
 }
 
 updateSystem
