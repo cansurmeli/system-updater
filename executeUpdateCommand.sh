d1fe0dfd3cb1ce288105a268f340ff8d84865dc8
Can Sürmeli <c_surmeli@icloud.com>
2019-06-18 09:02:02 +0200

    Implemented update command execution to account for the command availability. However, the availability check is yet to be implemented.

--- executeUpdateCommand.sh
+++ executeUpdateCommand.sh
@@ -1,5 +1,12 @@
 executeUpdateCommand() {
-	$1	
-	checkCommandStatus
-	printNewLine
+	isCommandAvailable = checkCommandAvailability $1
+
+	if [ "$isCommandAvailable" == 1]
+	then
+		$1	
+		checkCommandStatus
+		printNewLine
+	else
+		printWarningMessage "Command not available"
+	fi
 }
