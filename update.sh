4084aa0ba45f606ca76f855ccfc577a9181984ab
Can Sürmeli <cansurmeli@outlook.com>
2019-11-27 09:15:28 +0100

    File naming scheme change.
    Renamed the helper files/functions to be hidden to clear up the clatter
    and make the file hierarchy a little bit easier to read.

--- update.sh
+++ update.sh
@@ -2,23 +2,23 @@
 
 ARGUMENTS=$@
 
-source printNewLine.sh
-source printErrorMessage.sh
-source printStatusMessage.sh
-source printWarningMessage.sh
-source checkCommandAvailability.sh
-source checkCommandStatusAfterExecution.sh
-source checkSudoPrivileges.sh
-source executeUpdateCommand.sh
-source grabSudoIfNeeded.sh
-source handleHomebrew.sh
-source handleMacOS.sh
-source handleMasApps.sh
-source handlePIP.sh
-source handleRuby.sh
-source keepSudoPrivileges.sh
-source parseCommandLineArguments.sh
-source trapping.sh
+source .printNewLine.sh
+source .printErrorMessage.sh
+source .printStatusMessage.sh
+source .printWarningMessage.sh
+source .checkCommandAvailability.sh
+source .checkCommandStatusAfterExecution.sh
+source .checkSudoPrivileges.sh
+source .executeUpdateCommand.sh
+source .grabSudoIfNeeded.sh
+source .handleHomebrew.sh
+source .handleMacOS.sh
+source .handleMasApps.sh
+source .handlePIP.sh
+source .handleRuby.sh
+source .keepSudoPrivileges.sh
+source .parseCommandLineArguments.sh
+source .trapping.sh
 
 initiateSystemUpdate() {
 	parseCommandLineArguments "$ARGUMENTS"
